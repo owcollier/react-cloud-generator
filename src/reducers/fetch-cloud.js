a400f31b99eb5f6c3e8ad14240dbabb5c9a9a06c
Owcollier <owcollier@gmail.com>
2018-01-13 13:41:23 -0500

    refactoring of reducers to accomodate a combineReducers object underway

---
+++ src/reducers/fetch-cloud.js
@@ -0,0 +1,114 @@
+const initialState = {
+  view: 'home',
+  clouds: [],
+  activeCloud: {
+    title: '',
+    text: '',
+    words: [],
+    font: 'sans-serif',
+    color: 'random',
+    upvotes: 0,
+    downvotes: 0
+  },
+  loading: false,
+  error: null
+};
+
+export const fetchCloud = (state = initialState, action) => {
+  switch (action.type ) {
+    case 'FETCH_CLOUDS_REQUEST':
+      return Object.assign({}, state, {
+        loading: true,
+        error: null
+      })
+    case 'FETCH_CLOUDS_SUCCESS':
+      return Object.assign({}, state, {
+        loading: false,
+        error: null,
+        clouds: action.clouds,
+        view: 'library',
+        activeCloud: {
+          title: '',
+          text: '',
+          words: [],
+          font: 'sans-serif',
+          color: 'random',
+          upvotes: 0,
+          downvotes: 0
+        }
+      })
+    case 'FETCH_CLOUDS_ERROR':
+      return Object.assign({}, state, {
+        error: action.error,
+        loading: false
+      })
+    case 'FETCH_SINGLE_CLOUD_REQUEST':
+      return Object.assign({}, state, {
+        loading: true,
+        error: null
+      })
+    case 'FETCH_SINGLE_CLOUD_SUCCESS':
+      return Object.assign({}, state, {
+        loading: false,
+        error: null,
+        activeCloud: action.activeCloud,
+        view: 'focus'
+      })
+    case 'FETCH_SINGLE_CLOUD_ERROR':
+      return Object.assign({}, state, {
+        error: action.error,
+        loading: false
+      })
+    default: return state;
+  }
+}
+
+// else if(action.type === FETCH_CLOUDS_REQUEST){
+//   return Object.assign({}, state, {
+//     loading: true,
+//     error: null
+//   })
+// }
+// else if(action.type === FETCH_CLOUDS_SUCCESS){
+//   return Object.assign({}, state, {
+//     loading: false,
+//     error: null,
+//     clouds: action.clouds,
+//     view: 'library',
+//     activeCloud: {
+//       title: '',
+//       text: '',
+//       words: [],
+//       font: 'sans-serif',
+//       color: 'random',
+//       upvotes: 0,
+//       downvotes: 0
+//     }
+//   })
+// }
+// else if(action.type === FETCH_CLOUDS_ERROR){
+//   return Object.assign({}, state, {
+//     error: action.error,
+//     loading: false
+//   })
+// }
+// else if(action.type === FETCH_SINGLE_CLOUD_REQUEST){
+//   return Object.assign({}, state, {
+//     loading: true,
+//     error: null
+//   })
+// }
+// else if(action.type === FETCH_SINGLE_CLOUD_SUCCESS){
+//   return Object.assign({}, state, {
+//     loading: false,
+//     error: null,
+//     activeCloud: action.activeCloud,
+//     view: 'focus'
+//   })
+// }
+// else if(action.type === FETCH_SINGLE_CLOUD_ERROR){
+//   return Object.assign({}, state, {
+//     error: action.error,
+//     loading: false
+//   })
+// }
