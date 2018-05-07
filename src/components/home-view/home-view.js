bf792ba4ae3216a4db50171a107d0524dd6fcc4d
Owcollier <owcollier@gmail.com>
2018-05-07 11:09:47 -0400

    css updates for home-view, class added for home view description p

--- src/components/home-view/home-view.js
+++ src/components/home-view/home-view.js
@@ -9,7 +9,8 @@ export function HomeView(props) {
   return (
     <section className="home-view">
       <div className="home-description">
-        <p>Welcome to React Cloud Generator, a simple word cloud generator built with <a className="home-description-link">React.js</a> and the <a className="home-description-link">react-tag-cloud</a> library</p>
+        <p className="home-description-p">Welcome to React Cloud Generator, a simple word cloud generator built with <a className="home-description-link">React.js</a>
+         and the <a className="home-description-link">react-tag-cloud</a> library</p>
       </div>
       <div className="footer-link-a">
         <a className="library" onClick={() => props.dispatch(fetchClouds())}>Browse the Word Cloud Library</a>
