d712e6a43209f18374c411e0c9c83b61ef6280d9
Owcollier <owcollier@gmail.com>
2018-05-07 10:25:25 -0400

    tweaks to home-view component

--- src/components/home-view/home-view.js
+++ src/components/home-view/home-view.js
@@ -8,7 +8,9 @@ import { fetchClouds } from '../../actions/actions';
 export function HomeView(props) {
   return (
     <section className="home-view">
-      <p>Welcome to React Cloud Generator!</p>
+      <div className="home-description">
+        <h2>Welcome to React Cloud Generator!</h2>
+      </div>
       <div className="footer-link-a">
         <a className="library" onClick={() => props.dispatch(fetchClouds())}>Browse the Word Cloud Library</a>
       </div>
