02432290e96d5ac3a2eb90a97e37f33a425eefe8
Owcollier <owcollier@gmail.com>
2018-05-10 23:28:14 -0400

    updated content in home-view.js component

--- src/components/home-view/home-view.js
+++ src/components/home-view/home-view.js
@@ -14,7 +14,9 @@ export function HomeView(props) {
           <br></br><br></br>
           Using the Create feature, you can generate word clouds based on text input and customize the result using a variety of color palettes and font families. When you select Create in the navigation menu, you will be provided with a word cloud generator form that looks something like the example displayed below:
           <br></br><br></br>
-          Insert text into the form provided, provide a title for the word cloud, and select from additional stylistic options - once the form is complete, select the "Generate Word Cloud" button to create your word cloud.
+          Insert text into the form provided, provide a title for the word cloud, and select from additional stylistic options - once the form is complete, click the "Generate Word Cloud" button to create your word cloud.
+          <br></br><br></br>
+          And that's about all there is to it! Select Library in the navigation menu to view all user created word clouds. By selecting a given word cloud from the library, you can view, edit, and delete the word cloud. Enjoy using React Cloud Generator!
         </p>
       </div>
       <div className="footer-link-a">
