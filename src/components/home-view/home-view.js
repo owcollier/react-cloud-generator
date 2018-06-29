6e386b261aff016ddc66d1def3dcd04714cedeb1
Owcollier <owcollier@gmail.com>
2018-06-28 20:23:29 -0400

    added img tags to jsx in home-view component, added assets folder in src and image files

--- src/components/home-view/home-view.js
+++ src/components/home-view/home-view.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 
 import './home-view.css';
 
+import imageOne from '../../assets/reactCloudGenOne.png';
+import imageTwo from '../../assets/reactCloudGenTwo.png';
+import imageThree from '../../assets/reactCloudGenThree.png';
+
 import { fetchClouds } from '../../actions/actions';
 
 export function HomeView(props) {
@@ -14,7 +18,13 @@ export function HomeView(props) {
           <br></br><br></br>
           Using the Create feature, you can generate word clouds based on text input and customize the result using a variety of color palettes and font families. When you select Create in the navigation menu, you will be provided with a word cloud generator form that looks something like the example displayed below:
           <br></br><br></br>
+          <img src={imageOne} alt=""/>
+          <br></br><br></br>
           Insert text into the form provided, provide a title for the word cloud, and select from additional style options - once the form is complete, click the "Generate Word Cloud" button to create your word cloud.
+          <br></br><br></br>
+          <img src={imageTwo} alt=""/>
+          <br></br><br></br>
+          <img src={imageThree} alt=""/>
           <br></br><br></br>
           And that's about all there is to it! Select Library in the navigation menu to view all user created word clouds. By selecting a given word cloud from the library, you can view, edit, and delete the word cloud. Enjoy using React Cloud Generator!
         </p>
