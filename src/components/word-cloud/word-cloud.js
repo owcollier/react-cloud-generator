c814cefaf80c0de530e45e02422f17d20d2f16a6
Owcollier <owcollier@gmail.com>
2018-08-09 20:09:59 -0400

    testing inclusion of react-size-aware component as wrapper for rendered wordcloud

--- src/components/word-cloud/word-cloud.js
+++ src/components/word-cloud/word-cloud.js
@@ -5,6 +5,7 @@ import GeneratorForm from '../generator-form/generator-form';
 // import {goEdit, downVoteCloud, upVoteCloud} from '../../actions/actions';
 import {goEdit} from '../../actions/actions';
 import TagCloud from 'react-tag-cloud';
+import ResizeAware from 'react-resize-aware';
 import ReactWordCloud from 'react-wordcloud';
 import randomColor from 'randomcolor';
 import './word-cloud.css';
@@ -55,27 +56,27 @@ export class Wordcloud extends React.Component {
         return (
             <div className="word-cloud-outer">
                 <div className='word-cloud-inner' style={cloudFontStyle}>
-                    <div className="absolute-cloud-container" style={{width: 600, height: 400}}>
-                    <ReactWordCloud
-                        words={words}
-                        wordCountKey={WORD_COUNT_KEY}
-                        wordKey={WORD_KEY}
-                    />
-                        {/* <TagCloud 
-                            className="tag-cloud"
-                            style={{
-                            fontSize: 30,
-                            fontWeight: 'bold',
-                            fontStyle: 'italic',
-                            fontFamily: this.props.activeCloud.font,
-                            color: () => randomColor({
-                                hue: this.props.activeCloud.color
-                            }),
-                            padding: 5
-                            }}>
-                            {words}
-                        </TagCloud> */}
-                    </div>
+                    <ResizeAware>
+                        <ReactWordCloud
+                            words={words}
+                            wordCountKey={WORD_COUNT_KEY}
+                            wordKey={WORD_KEY}
+                        />
+                            {/* <TagCloud 
+                                className="tag-cloud"
+                                style={{
+                                fontSize: 30,
+                                fontWeight: 'bold',
+                                fontStyle: 'italic',
+                                fontFamily: this.props.activeCloud.font,
+                                color: () => randomColor({
+                                    hue: this.props.activeCloud.color
+                                }),
+                                padding: 5
+                                }}>
+                                {words}
+                            </TagCloud> */}
+                    </ResizeAware>
                     {this.props.view === 'focus' && <div className="edit-form">
                         <form onSubmit={(event) => this.onSubmit(event)}>
                             <button type="submit" name="submit" id="editWordCloud" className="button">
